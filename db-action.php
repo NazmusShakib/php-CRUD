b447abb038b3ecc9c8c1abfc4248150486cc1550
Nazmus Shakib <mdshuvobc@gmail.com>
2016-09-26 16:14:22 +0600

    Database design

--- db-action.php
+++ db-action.php
@@ -1,31 +1,50 @@
 <?php
-
-include('db_connection.php');
+include('db-connect.php');
 
 $get_data =  $_POST['id'];
-
-
 $jsonData = json_decode($get_data,true);
-
 $length = count($jsonData);
 
+echo "<hr/>";
+
 for ($i=0; $i<$length; $i++) {
-  print $jsonData[$i].'<br>';
+$j=0;
+	$query = "INSERT INTO menus (menu_id, name, parent_id, position) VALUES ({$jsonData[$i]['id']},'Menu Name-{$i}', null, {$i})";
+	echo "Parent Query: ".$query."<br/>";
+
+
+  	if(isset($jsonData[$i]['children'])){
+  		foreach ($jsonData[$i]['children'] as $child) {
+  			$k=0;
+  			$query = "INSERT INTO menus (menu_id, name, parent_id, position) VALUES ({$child['id']},'Child of-{$jsonData[$i]['id']}', {$jsonData[$i]['id']}, {$j})";
+
+  			echo "&nbsp;&nbsp;&nbsp;&nbsp; Child Query: ".$query."<br/>";
+  			$j++;
+
+		  	if(isset($child['children'])){
+		  		foreach ($child['children'] as $baccha) {
+
+		  			$query = "INSERT INTO menus (menu_id, name, parent_id, position) VALUES ({$baccha['id']},'Child of-{$child['id']}', {$child['id']}, {$k})";
+
+		  			echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Child Query: ".$query."<br/>";
+		  			$k++;
+		  		}
+			}
+
+  		}
+  }
+
 }
 
-
-/*exit();
-for ($i=0; $i<$arr; $i++) { 
-	echo $jsonData[$i]['id'].;
-}*/
-
-/*print_r ($jsonData) ;*/
-
-
-
-
-/*for($i=1; $i<=4; $i++){
-	echo "<pre>";
-	print_r(json_decode($get_data[$i]['id']));
-	echo "</pre>";
-}*/
+/*$query = "INSERT INTO menus (name, parent_id, order_no) VALUES ('menu_name','{$phone}','{$jsonData[$i]}')";
+		// Perform queries 
+		$per = mysqli_query($link, $query);
+		if(!$per){
+			mysqli_close($link);
+			echo $query;
+			die("\nInsert Failed");	
+		}else{
+			mysqli_close($link);
+			echo ("Insert Successfully.");
+		}
+*/
