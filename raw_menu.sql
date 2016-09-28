16c43d84f7a5694e644c27777b3f657635e3bd93
Nazmus Shakib <mdshuvobc@gmail.com>
2016-09-28 09:07:59 +0600

    add database file

--- raw_menu.sql
+++ raw_menu.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 27, 2016 at 07:57 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 7.0.9
+-- Host: localhost
+-- Generation Time: Sep 28, 2016 at 09:07 AM
+-- Server version: 5.7.13-0ubuntu0.16.04.2
+-- PHP Version: 7.0.8-0ubuntu0.16.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,28 +31,29 @@ CREATE TABLE `menus` (
   `menu_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `parent_id` int(11) DEFAULT NULL,
-  `position` int(11) NOT NULL
+  `position` int(11) NOT NULL,
+  `menu_order` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `menus`
 --
 
-INSERT INTO `menus` (`id`, `menu_id`, `name`, `parent_id`, `position`) VALUES
-(141, 1, 'Item 1', NULL, 0),
-(142, 2, 'Item 2', NULL, 0),
-(143, 3, 'Item 3', 2, 1),
-(144, 4, 'Item 4', 2, 1),
-(145, 5, 'Item 5', 2, 1),
-(146, 6, 'Item 6', 5, 2),
-(147, 7, 'Item 7', 5, 2),
-(148, 8, 'Item 8', 5, 2),
-(149, 9, 'Item 9', 2, 1),
-(150, 10, 'Item 10', 2, 1),
-(151, 11, 'Item 11', NULL, 0),
-(152, 12, 'Item 12', NULL, 0),
-(153, 13, 'Item 13', NULL, 0),
-(154, 14, 'Item 14', NULL, 0);
+INSERT INTO `menus` (`id`, `menu_id`, `name`, `parent_id`, `position`, `menu_order`) VALUES
+(4, 1, 'Item 1', 0, 0, 4),
+(5, 2, 'Item 2', 1, 1, 0),
+(6, 3, 'Item 3', 0, 0, 2),
+(7, 4, 'Item 4', 13, 2, 0),
+(8, 5, 'Item 5', 2, 2, 0),
+(9, 6, 'Item 6', 5, 2, 0),
+(10, 7, 'Item 7', 5, 2, 1),
+(11, 8, 'Item 8', 5, 2, 2),
+(12, 9, 'Item 9', 2, 2, 1),
+(13, 10, 'Item 10', 2, 2, 2),
+(14, 11, 'Item 11', 0, 0, 1),
+(15, 12, 'Item 12', 0, 0, 3),
+(16, 13, 'Item 13', 11, 1, 0),
+(17, 14, 'Item 14', 0, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -72,7 +73,7 @@ ALTER TABLE `menus`
 -- AUTO_INCREMENT for table `menus`
 --
 ALTER TABLE `menus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=155;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
